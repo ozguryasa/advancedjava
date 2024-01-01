d8875b7f4167d30dca9ae111211f458bfe1906c0
ozguryasa <ozguryasa@gmail.com>
2024-01-01 20:01:55 +0300

    test Change

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -3,6 +3,6 @@ package app;
 public class App{
     public static void main(String [] args)
     {
-        System.out.println("Hello");
+        System.out.println("Hello in test branch");
     }
 }
